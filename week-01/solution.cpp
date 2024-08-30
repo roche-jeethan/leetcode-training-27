3417bb34c994c5bed5f011cd02cfdbc905bed636
Jeethan Roche <jeethanroche@gmail.com>
2024-08-30 16:53:49 +0530

    Completion Foo Bar Baz

--- week-01/solution.cpp
+++ week-01/solution.cpp
@@ -5,6 +5,14 @@ int main(){
     std::cout<<"Enter the range: \n";
     std::cin>>a>>b;
     for(int i=a;i<=b;i++){
-        
+        if(i%3==0){
+            std::cout<<"Foo"<<"\n";
+        }
+        else if(i%3!=0 && i%2==0){
+            std::cout<<"Bar"<<"\n";
+        }
+        else if(i%2!=0 && i%3!=0){
+            std::cout<<"Baz"<<"\n";
+        }
     }
 }
